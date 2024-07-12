38de2e4de71f30365119b73935f7620e89dd2f14
faaarce <risfararie@gmail.com>
2024-07-12 07:45:07 +0700

    Initial Commit

---
+++ Exercise.playground/Contents.swift
@@ -0,0 +1,65 @@
+import Cocoa
+
+enum InventoryError: Error {
+  case outOfStock, invalidCategory, expiredProduct
+}
+
+extension String {
+  
+}
+
+protocol Discountable {
+  func applyDiscount(_ percentage: Double)
+}
+
+class Product: Discountable {
+  let name: String
+  let category: String
+  let price: Double
+  let quantity: Int
+  
+  init(name: String, category: String, price: Double, quantity: Int) {
+    self.name = name
+    self.category = category
+    self.price = price
+    self.quantity = quantity
+  }
+  
+  func applyDiscount(_ percentage: Double) {
+    
+  }
+  
+  func productInfo() -> String {
+    "it's a \(name) from \(category) with \(price) and \(quantity)"
+  }
+  
+  
+}
+
+
+class PerishableProduct: Product {
+  
+  let expirationDate: Date
+  
+  init(expirationDate: Date, name: String, category: String, price: Double, quantity: Int) {
+    self.expirationDate = expirationDate
+    super.init(name: name, category: category, price: price, quantity: quantity)
+  }
+  
+  override func applyDiscount(_ percentage: Double) {
+    
+  }
+  
+  override func productInfo() -> String {
+    "it's a \(name) from \(category) with \(price) and \(quantity)"
+  }
+}
+
+
+class InventoryManager {
+  func sellProduct(_ product: Product, quantity: Int) throws {
+    
+  }
+}
+
+
